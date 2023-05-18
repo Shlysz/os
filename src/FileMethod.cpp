a17066e96d4097618a390fe5c0a525fb951938e6
shlysz <2665683651@qq.com>
2023-05-18 16:51:03 +0800

    文件系统和shell的完善

---
+++ src/FileMethod.cpp
@@ -0,0 +1,48 @@
+//
+// Created by 26656 on 2023/5/18.
+//
+
+#include "FileMethod.h"
+
+#include <utility>
+#include <sstream>
+#include "global.h"
+bool FileMethod::writeByte(string name) {
+    File*file=fs->open(std::move(name),1);
+    if(file== nullptr) return  false;
+    //先使用stream流接受输入，然后赋值到char*上，并计算长度
+    string buffer;
+    stringstream ss;
+    cout<<"please input your data:"<<endl;
+    while(getline(cin,buffer)){
+        ss<<buffer<<endl;
+    }
+    string input=ss.str();
+    char*data=new char[input.length()];
+    strcpy(data,input.c_str());
+    if(!fs->write(file,data,input.length())) {
+        fs->close(file);
+        delete[] data;
+        data= nullptr;
+        return false;
+    }
+
+    return true;
+}
+
+int FileMethod::readByte(string name) {
+    File*file=fs->open(std::move(name),0);
+    if(file== nullptr) return  -1;
+    //得到文件的长度
+    int length=file->inode->size;
+    char*data=new char[length];
+if(!fs->read(file,data)) {
+        fs->close(file);
+        delete[] data;
+        data= nullptr;
+        return -1;
+    }
+    cout<<"your data is:"<<endl;
+    cout<<data<<endl;
+    return length;
+}
--- src/shell.h
+++ src/shell.h
@@ -9,4 +9,5 @@
 class Shell {
    public:
     int parse();  // parse the parameter
+
 };
