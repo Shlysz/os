1b6d080b62d1ac5d71d447d8332ee92f1a830860
mhy <3298807098@qq.com>
2023-05-18 17:37:14 +0800

    some changes

---
+++ src/memory.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "memory.h"
+using namespace std;
+byte mem[memory_size];
+byte disk[disk_size];
+
+
+
+
+
+void pagetable::init_pagetable(){
+    pagetable pt;
+    pt.isallocated = 1;
+
+
+}
