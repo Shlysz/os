e1acdd920ea6a5a712b0a2e348cabe791f0c794e
mhy <3298807098@qq.com>
2023-05-20 21:12:52 +0800

    update memory.cpp and add MMU in memory.h

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
 #include "memory.h"
 using namespace std;
 BYTES mem[memory_size];
-int remaining_memory = memory_size;
+
+int pagetableitem = 1024;//It means a pagetable has less than 1024 items ,we use array[] to store it 
+int tlbitem = 32; //It means a TLB has less than 1024 items ,we use array[] to store it
+int remain_mem = memory_size;// to calculate the memory that can be used
+int tlb_id = 0;
+int pagetable_id = 0;
 //byte disk[disk_size];
 
 struct v_address{
@@ -15,15 +19,34 @@ struct p_address{
     int offfset;
 };
 
-void Pagetable::init_pagetable(){
-    Pagetable pt;
-    pt.isallocated = 1;
+Pagetable::Pagetable(int pid){
+    this->pagetable_id = pid;
     /*
-    get pid, set pt.pagetable_id = pid;
-    randomly create pageitems
+    get pid, set pagetable_id = pid;
+    randomly create pageitems;
+    */
+
+
+}
+
+TLB::TLB(int pid){
+    this->tlb_id = pid;
+    /*
+    get pid,set tlb_id = pid;
+    randomly create tlb items;
     */
 
 }
 
 
+
+
+
+
+
+
+
+int Query_memory(){
+    return remain_mem;
+}
 
