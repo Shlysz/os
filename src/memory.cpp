a90dabc6958d60f70a8b924e7ab4a95e0cab26da
mhy <3298807098@qq.com>
2023-05-22 16:18:58 +0800

    add some functions

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,12 +1,14 @@
 #include "memory.h"
+#include "global.h"
 using namespace std;
 BYTES mem[memory_size];
 
 int pagetableitem = 1024;//It means a pagetable has less than 1024 items ,we use array[] to store it 
 int tlbitem = 32; //It means a TLB has less than 1024 items ,we use array[] to store it
-int remain_mem = memory_size;// to calculate the memory that can be used
+int mem_available = memory_size;// to calculate the memory that can be used
 int tlb_id = 0;
 int pagetable_id = 0;
+MMU::Mlist *mlist = nullptr;
 //byte disk[disk_size];
 
 struct v_address{
@@ -18,65 +20,93 @@ struct p_address{
     int offfset;
 };
 
-Pagetable::Pagetable(int pid){
-    this->pagetable_id = pid;
-
-    this->page_phyaddr = new int[pagetablesize];
-    for(int i = 0;i < pagetablesize;i++){
-        page_phyaddr[i] = pid * 64 + i;
-        if(page_phyaddr[i] >1023||page_phyaddr[i]<0){
-            cout << "Error:Out of paging range";
-            //INTERRUPT:STORTAGE OF PAGES??
-        } 
-    }
+Pagetable Pagetable::initPagetable(int pid){
+    Pagetable pt; 
+    pt.pagetable_id = pid;
+    pt.page_phyaddr = new int[pagetablesize];
+    pt.is_changed = new int[pagetablesize];
+    pt.is_inmemory = new int[pagetablesize];
+    pt.last_used = new int[pagetablesize];
+    memset(is_changed,0,pagetablesize);
+    memset(is_inmemory,0,pagetablesize);
+    memset(last_used,0,pagetablesize);
+    for(int i = 0;i < pagetablesize;i++) page_phyaddr[i] = pid * 64 + i;
+    return pt;
 }
 
 void Pagetable::release_pagetable(Pagetable pt){
     delete[] pt.page_phyaddr;
+    delete[] pt.is_changed;
+    delete[] pt.is_inmemory;
+    delete[] pt.last_used;
 }
 
 
-TLB::TLB(int pid){
-    this->tlb_id = pid;
-    this->TLBitem = new int[TLBsize];
-    for(int i = 0;i<TLBsize;i++){
-        TLBitem[i] = pid * 64 + i;
+TLB TLB::initTLB(int pid){
+    TLB tlb;
+    tlb.tlb_id = pid;
+    tlb.page_phyaddr = new int[tlbsize];
+    tlb.is_changed = new int[tlbsize];
+    tlb.is_inmemory = new int[tlbsize];
+    tlb.last_used = new int[tlbsize];
+    memset(is_changed,0,tlbsize);
+    memset(is_inmemory,0,tlbsize);
+    memset(last_used,0,tlbsize);
+    for(int i = 0;i < tlbsize;i++){
+        page_phyaddr[i] = pid * 64 + i;
     }
+    return tlb;
 }
 void TLB::release_tlb(TLB tlb){
-    delete[] tlb.TLBitem;
+    delete[] tlb.page_phyaddr;
+    delete[] tlb.is_changed;
+    delete[] tlb.is_inmemory;
+    delete[] tlb.last_used;
 }
+
+
 
 
 MMU::MMU(){
     this->framearray = new int[total_frame] ;
     std::memset(framearray, 0, sizeof(framearray));
-
     //cout<<"MMU START"<<endl;
 }
 
 void MMU::Memory_allocate(int pid){ 
-
-
-
+    mlist->mid = pid;
+    mlist->PT = initpt(pid);
+    mlist->TLb = initlb(pid); 
+    mem_available -= 64 * 1024;
+    if(mem_available < 1){
+        std::cout << "Error:No Enough Memory!" << std::endl;
+    }
+    //MMU:PT(pid);
 
 }
 void MMU::Memory_release(int pid){
 
+    
+    while(mlist){
+        
+
+
+
+    }
 
 
 }
 
-void Page_replace(int pid){
+void MMU::LRU_replace(){
     /*
-    opt 最佳页面置换算法
+    LRU least recently used
     */
 
 
 }
 
 
-void Find_paddress(int pid,TLB tlb,Pagetable pt){
+void Find_phyaddr(int pid,TLB tlb,Pagetable pt){
 
 
 
--- src/memory.h
+++ src/memory.h
@@ -7,11 +7,11 @@ unsigned int ---> virtual address,physical address 32bits,
 while the page/frame number is 20 bits and the offset is 12 bits
 memory size ---> 4MB
 use 0/1 to simulate whether a memory unit is used
-TLB item num:32
-pagetable item num: 1024
+TLB item num:8
+pagetable item num: 64
 for each process ,init a pagetable and a TLB,and when a process is terminated, release it 
 */
-#define TLBsize 8 // 8B
+#define tlbsize 8 // 8B
 #define pagetablesize 64 // 64B a pagetable has 64 pageitem
 #define memory_size 4 * 1024 * 1024 //1 = 1B ,total:4MB 
 #define pagesize = 4 * 1024
@@ -27,11 +27,11 @@ when the process is terminated, release it
 class Pagetable{
     public:
     int pagetable_id;  
-    int * page_phyaddr = nullptr;
+    int * page_phyaddr = nullptr; //frame address
     int * last_used = nullptr;
+    int * is_inmemory = nullptr;
     int * is_changed = nullptr;
-    
-    Pagetable(int pid);  //Init a pagetable when a process is created
+    Pagetable initPagetable(int pid);  //Init a pagetable when a process is created
     void release_pagetable(Pagetable pt);//Release it when the process is terminated
 };
 
@@ -41,14 +41,58 @@ tlb_id = pid
 when a process is created, init a tlb and randomly create a TLBitem[] and simulate addressing from v to p
 when the process is terminated, release it   
 */
+
 class TLB{
     public:
     int tlb_id;
-    int *TLBitem = nullptr;
-
-    TLB(int pid);//Init a TLB when a process is created
+    int * page_phyaddr = nullptr; //frame 
+    int * last_used = nullptr;
+    int * is_inmemory = nullptr;
+    int * is_changed = nullptr;
+    TLB initTLB(int pid);//Init a TLB when a process is created
     void release_tlb(TLB tlb);//Release it when the process is terminated
 };
+
+/*
+MMU: memory manage unit 
+Functions: allocate memory for process,release used memory,transfer vad to pad,return memory that can be used 
+*/
+
+class MMU{
+
+    private:
+    // Pagetable PT;
+    // TLB TLb;
+
+    public:
+    int total_frame = 1024;
+    int total_memory = memory_size;
+    int *framearray = nullptr; //pid in it
+    typedef struct MMUlist{
+        int mid; //mid = pid
+        Pagetable PT;
+        TLB TLb;
+        struct MMUlist *next ;
+    }Mlist;
+    
+    MMU();
+    //void IO_in_memory();
+    Pagetable initpt(int pid){
+        Pagetable p;
+        return p.initPagetable(pid);
+        }    
+    TLB initlb(int pid){
+        TLB t;
+        return t.initTLB(pid);
+    }    
+    void Memory_allocate(int pid);
+    void Memory_release(int pid);
+    void LRU_replace();
+    void Find_phyaddr(int pid,TLB tlb,Pagetable pt);
+    void Query_memory();
+    
+};
+
 
 /*
 Page_num range: 0~1023
@@ -76,29 +120,3 @@ if a Frame is allocated, isused = 1,in the meanwhile the memory need - 4*1024
 //     void frame_init(int remaining_memory);
 //     void frame_release(int remaining_memory); 
 // };
-/*
-MMU: memory manage unit 
-Functions: allocate memory for process,release used memory,transfer vad to pad,return memory that can be used 
-*/
-
-class MMU{
-
-    public:
-    int total_frame = 1024;
-    int total_memory = memory_size;
-    int *framearray = nullptr;
-    
-
-
-    MMU();
-
-    //void IO_in_memory();
-    void Memory_allocate(int pid);
-    void Memory_release(int pid);
-    void OPT_replace();
-    void Find_paddress(int pid,TLB tlb,Pagetable pt);
-    void Query_memory();
-    
-};
-
-
