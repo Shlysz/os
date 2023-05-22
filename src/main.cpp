32f5dddb5a97c208ccf10150409af0eefaf7d2fa
livrth <lyvrth@gmail.com>
2023-05-22 20:06:28 +0800

    feat: interupt info queue

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,7 @@
 
 int main() {
     int ret = kernel.kernel_init();
-    if (!ret) 
-        cout << "kernel init wrong!" << endl;
+    if (!ret) cout << "kernel init wrong!" << endl;
 
     // system("clear");
     UI menu;
