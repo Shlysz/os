4763c59c8f14864d8b43719531bbcbcb62cfbad6
livrth <lyvrth@gmail.com>
2023-05-24 15:34:55 +0800

    update

--- src/interupt_test.cpp
+++ src/interupt_test.cpp
@@ -1,5 +1,6 @@
 #include "interupt.h"
 
+#include "device.h"
 #include "shell.h"
 #include "ui.h"
 
@@ -10,17 +11,25 @@ int test() {
     Interupt inte3, inte4, inte5;
     Interupt inte6;
     Interupt inte7;
-    inte1.raise_time_interupt(55);
-    inte2.raise_time_interupt(55);
-    inte3.raise_time_interupt(55);
-    inte4.disable_time_interupt(55);
-    inte5.disable_time_interupt(55);
 
-    while (!process_info_queue.empty()) {
-        auto t = process_info_queue.front();
-        process_info_queue.pop();
-        cout << t.first << ' ' << t.second << endl;
-    }
+    device tmp;
+    tmp.init_device();
+
+    inte1.raise_device_interupt(2, 0);  // pid =2, deviceid = 0;
+    inte2.raise_device_interupt(99, 1);
+    inte3.raise_device_interupt(88, 2);
+    inte3.raise_device_interupt(100, 1);
+    inte1.disable_device_interupt(2, 0);
+    inte1.disable_device_interupt(1, 1);
+    inte1.disable_device_interupt(99, 1);
+
+    tmp.show_device_all();
+
+    // while (!process_info_queue.empty()) {
+    //     auto t = process_info_queue.front();
+    //     process_info_queue.pop();
+    //     cout << t.first << ' ' << t.second << endl;
+    // }
 
     return 0;
 }
